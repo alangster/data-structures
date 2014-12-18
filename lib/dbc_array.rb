56fa3226b9525d6e2e254cad8dc40de34d1b0592
Alex Lang <langalex4@gmail.com>
2014-12-17 18:05:13 -0600

    still a bug with the #at method, but passing all specs except one

--- lib/dbc_array.rb
+++ lib/dbc_array.rb
@@ -2,6 +2,41 @@ require 'empty_exception'
 
 class DbcArray
 
-  
+	attr_reader :collection
+
+  def initialize
+  	@collection = []
+  end
+
+  def empty?
+  	collection.count == 0
+  end
+
+  def size
+  	collection.count
+  end
+
+  def insert(elem, at: 0)
+		pos = at
+  	if at != 0
+  		pos = collection.index(at) 
+  	end
+  	@collection = collection[0...pos] + [elem] + collection[pos..-1]
+  end
+
+  def at(pos, put: nil)
+  	raise ::OutOfBoundException unless (0..collection.length).include?(pos.abs) #using collection.length so that elements can be inserted at the end
+  	collection[pos] = put if put
+  	collection[pos]
+  end
+
+  def append(elem)
+  	@collection << elem
+  	self
+  end
+
+  def remove_at(pos)
+  	@collection = collection[0...pos] + collection[(pos + 1)..-1]
+  end
 
 end
