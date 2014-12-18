d18bd927604d27f5c68b23b812a4bf5dc314e066
Alex Lang <langalex4@gmail.com>
2014-12-17 18:08:55 -0600

    built queue, passing all specs

--- lib/dbc_queue.rb
+++ lib/dbc_queue.rb
@@ -2,5 +2,25 @@ require 'empty_exception'
 
 class DbcQueue
 
+	attr_reader :collection
+
+	def initialize
+		@collection = []
+	end
+
+	def empty?
+		# although i could just use #empty? on the collection array,
+		# I want to implement everything very literally
+		collection.count == 0
+	end
+
+	def enqueue(elem)
+		@collection << elem
+	end
+
+	def dequeue
+		raise ::EmptyException if empty?
+		@collection.shift
+	end
 
 end
