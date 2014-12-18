f45e2f2a04cd0c2068b092d7bdc40727d9f5a38f
Alex Lang <langalex4@gmail.com>
2014-12-17 20:45:31 -0600

    passing all specs properly

--- lib/dbc_circular_queue.rb
+++ lib/dbc_circular_queue.rb
@@ -3,38 +3,48 @@ require 'full_exception'
 
 class DbcCircularQueue
 
-	attr_reader :collection, :front, :rear
+	attr_reader :collection, :start_index, :end_index, :storage
 
 	def initialize(size)
-		@front = 0
-		@rear = size 
-		@collection = []  	
+		@start_index = 0
+		@end_index = 0
+		# need to increase storage by one so that it is possible to add items when
+		# the end is pointing to the final slot, otherwise, attempts to add items will
+		# raise a FullException--adding one serves as a buffer
+		@storage = size + 1 
+		@collection = Array.new(size)  	
 	end  
 
 	def empty?
-		collection.size == 0
+		start_index == end_index
 	end
 
 	def full?
-		front == rear
+		# need to check whether or not advancing the end will make it equal to the start
+		start_index == advance(end_index)
 	end
 
 	def enqueue(elem)
 		full_check
-		@collection << elem
-		@front += 1
+		# insert the element at the 'end', then advance the end
+		@collection[end_index] = elem
+		@end_index = advance(end_index)
 	end
 
 	def dequeue
 		empty_check
-		elem = collection[0]
-		@collection = collection[1..-1]
-		@front += 1
-		@rear += 1
+		elem = collection[start_index]
+		@start_index = advance(start_index)
+		
 		elem
 	end
 
 	private
+
+	def advance(index)
+		# once the index reaches the storage value, it will return 0
+		(index + 1) % storage
+	end
 
 	def empty_check
 		raise ::EmptyException if empty?
